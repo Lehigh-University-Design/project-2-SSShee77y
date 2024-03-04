8156dab4e6ef94fbee6417ffeca71ccc1d27799c
Leo Chen <46356943+SSShee77y@users.noreply.github.com>
2024-03-04 11:03:32 -0500

    Added first popup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 function toggleLightMode() {
     document.body.classList.toggle('light-mode');
 }
+
+popupCounter = 0
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var vhToPx = window.innerWidth / 100;
+    console.log(scrollPosition);
+
+    if (popupCounter == 0) { // Cookie settings
+        let popup = document.getElementById('cookie-settings');
+        if (scrollPosition > popup.offsetTop - popup.clientHeight)
+            document.body.style.overflow = 'hidden';
+    }
+});
+
+function acceptCookies() {
+    if (popupCounter < 1) {
+        document.body.style.overflow = 'auto';
+        popupCounter = 1;
+    }
+}
