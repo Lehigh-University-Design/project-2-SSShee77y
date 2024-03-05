966377b0c889e9af37dc0f843eafe0d0775a4238
Leo Chen <46356943+SSShee77y@users.noreply.github.com>
2024-03-05 17:31:04 -0500

    Added info form popup

--- js/script.js
+++ js/script.js
@@ -9,15 +9,37 @@ window.addEventListener('scroll', function() {
     console.log(scrollPosition);
 
     if (popupCounter == 0) { // Cookie settings
-        let popup = document.getElementById('cookie-settings');
-        if (scrollPosition > popup.offsetTop - popup.clientHeight)
+        let popup = document.getElementById('cookie-settings').parentElement;
+        if (scrollPosition >= popup.offsetTop) {
+            popup.classList.add('fixed-popup');
             document.body.style.overflow = 'hidden';
+        }
+    } else if (popupCounter == 1) { // Info form
+        let popup = document.getElementById('information-form').parentElement;
+        if (scrollPosition >= popup.offsetTop) {
+            popup.classList.add('fixed-popup');
+            document.body.style.overflow = 'hidden';
+        }
     }
 });
 
 function acceptCookies() {
-    if (popupCounter < 1) {
+    if (popupCounter <= 0) { // Cookie settings
+        let popup = document.getElementById('cookie-settings');
+        popup.classList.remove('fixed-popup');
         document.body.style.overflow = 'auto';
         popupCounter = 1;
+    } 
+}
+
+function submitForm(event) {
+    event.preventDefault();
+
+    if (popupCounter <= 1) { // Info form
+        let popup = document.getElementById('information-form');
+        popup.classList.remove('fixed-popup');
+        document.body.style.overflow = 'auto';
+        popupCounter = 2;
     }
+    return true;
 }
